d3d3da3b43f61c30d418b93c382b7197da7cb690
Gegcbr9319 <gegcbr9319@gmail.com>
2023-03-06 22:38:27 +0300

    init: start calculator

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+export function App() {
+    return (
+        <div>
+            <p>Start</p>
+        </div>
+    );
+}
+
+export default App;
